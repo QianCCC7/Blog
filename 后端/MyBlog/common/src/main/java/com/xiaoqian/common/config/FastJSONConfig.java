8ef62fa625921f4f9976e5e1612368a986d63063
xiaoqian <1805521303@qq.com>
2024-01-22 19:50:29 +0800

    配置FastJson控制返回的时间格式

---
+++ 后端/MyBlog/common/src/main/java/com/xiaoqian/common/config/FastJSONConfig.java
@@ -0,0 +1,36 @@
+package com.xiaoqian.common.config;
+
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class FastJSONConfig implements WebMvcConfigurer {
+
+    @Bean // 使用 @Bean注入 fastJsonHttpMessageConvert
+    public HttpMessageConverter<?> fastJsonHttpMessageConverters() {
+        // 1. 定义一个 Convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        SerializeConfig.globalInstance.put(Long.class, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(SerializeConfig.globalInstance);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return fastConverter;
+    }
+
+    // 配置消息转换器
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(fastJsonHttpMessageConverters());
+    }
+}
