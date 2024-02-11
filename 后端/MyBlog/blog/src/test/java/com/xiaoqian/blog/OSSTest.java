251147cd38f161a52907c2f2931eb5c320d5b0f0
xiaoqian <1805521303@qq.com>
2024-02-11 14:38:20 +0800

    整合七牛云的OSS对象存储服务

---
+++ 后端/MyBlog/blog/src/test/java/com/xiaoqian/blog/OSSTest.java
@@ -0,0 +1,57 @@
+package com.xiaoqian.blog;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+
+@SpringBootTest
+@Component
+@Data
+@ConfigurationProperties(prefix = "oss")
+public class OSSTest {
+    private String accessKey;
+    private String secretKey;
+    private String bucket;// 存储空间的名字
+
+    @Test
+    void testOss() throws FileNotFoundException {
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.autoRegion());
+        cfg.resumableUploadAPIVersion = Configuration.ResumableUploadAPIVersion.V2;// 指定分片上传版本
+        UploadManager uploadManager = new UploadManager(cfg);
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = "cat2.png";
+        InputStream inputStream = new FileInputStream("C:\\Users\\aifuxi\\Desktop\\cat.png");
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(inputStream, key, upToken, null, null);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            System.out.println(putRet.key);
+            System.out.println(putRet.hash);
+        } catch (QiniuException ex) {
+            ex.printStackTrace();
+            if (ex.response != null) {
+                System.err.println(ex.response);
+                try {
+                    String body = ex.response.toString();
+                    System.err.println(body);
+                } catch (Exception ignored) {
+                }
+            }
+        }
+    }
+}
