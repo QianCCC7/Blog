d95e3b056a87eeb469deb15406ced13a811763c0
xiaoqian <1805521303@qq.com>
2024-02-27 20:49:05 +0800

    修复定时任务更新浏览量时无法获取到认证信息导致的空指针bug

--- 后端/MyBlog/common/src/main/java/com/xiaoqian/common/handle/BaseMetaObjectHandler.java
+++ 后端/MyBlog/common/src/main/java/com/xiaoqian/common/handle/BaseMetaObjectHandler.java
@@ -1,11 +1,14 @@
 package com.xiaoqian.common.handle;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.xiaoqian.common.domain.pojo.User;
 import com.xiaoqian.common.utils.UserContext;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * 操作数据库前自动填充需要更新的内容，只支持单个对象，不支持批量插入更新时的填充
@@ -49,13 +52,19 @@ public class BaseMetaObjectHandler implements MetaObjectHandler {
     public void setUpdater(MetaObject metaObject) {
         Long userId;
         try {
-            // 当用户处于注册状态时，仍然需要字段填充 createBy字段，该字段会从 principal中获取
-            // 而注册的用户的 principal为字符串 "anonymousUser"，无法转换为 LoginUser对象，所以会爆错
-            // 此时只需要在字段填充时捕获一下异常，并且重新赋值 userId即可
-            userId = UserContext.getUserId();
+            // 当定时任务执行更新浏览量的操作时不需要 token，也就没有 Authentication，此时默认 userId = -1
+            Authentication authentication = UserContext.getAuthentication();
+            if (Objects.isNull(authentication)) {
+                userId = -1L;
+            } else {
+                // 当用户处于注册状态时，仍然需要字段填充 createBy字段，该字段会从 principal中获取
+                // 而注册的用户的 principal为字符串 "anonymousUser"，无法转换为 LoginUser对象，所以会爆错
+                // 此时只需要在字段填充时捕获一下异常，并且重新赋值 userId即可
+                userId = UserContext.getUserId();
+            }
         } catch (Exception e) {
             userId = -1L;
-            // e.printStackTrace();
+            e.printStackTrace();
         }
         this.setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
         this.setFieldValByName("updateBy", userId, metaObject);
