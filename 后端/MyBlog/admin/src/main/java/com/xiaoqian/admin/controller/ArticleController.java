702b6a8544ac548c9217b852254d8f7032826bdb
xiaoqian <1805521303@qq.com>
2024-03-03 17:15:34 +0800

    实现写博文时需要上传文章缩略图的接口

--- 后端/MyBlog/admin/src/main/java/com/xiaoqian/admin/controller/ArticleController.java
+++ 后端/MyBlog/admin/src/main/java/com/xiaoqian/admin/controller/ArticleController.java
@@ -4,31 +4,36 @@ import com.xiaoqian.common.domain.ResponseResult;
 import com.xiaoqian.common.domain.vo.CategoryVo;
 import com.xiaoqian.common.domain.vo.TagVo;
 import com.xiaoqian.common.service.IArticleService;
+import com.xiaoqian.common.service.IUploadService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Api(tags = "写博文相关接口")
 @RestController
-@RequestMapping("/content")
 @RequiredArgsConstructor
 public class ArticleController {
     private final IArticleService articleService;
+    private final IUploadService uploadService;
 
     @ApiOperation("查询可用的文章分类")
-    @GetMapping("/category/listAllCategory")
+    @GetMapping("/content/category/listAllCategory")
     public ResponseResult<List<CategoryVo>> queryAllCategories() {
         return articleService.queryAllCategories();
     }
 
     @ApiOperation("查询可用的文章标签")
-    @GetMapping("/tag/listAllTag")
+    @GetMapping("/content/tag/listAllTag")
     public ResponseResult<List<TagVo>> queryAllTags() {
         return articleService.queryAllTags();
+    }
+
+    @PostMapping("/upload")
+    public ResponseResult<Object> upload(@RequestPart("img")MultipartFile multipartFile) {
+        return uploadService.upload(multipartFile);
     }
 }
